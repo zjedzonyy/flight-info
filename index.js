af5d86f1b23798c8932dd8db3a4ca3e03a506b04
zjedzonyy <artompor@gmail.com>
2024-12-05 20:55:50 +0100

    Add country-based flight search functionality, refactor data retrieval, and filter flight information by country

--- index.js
+++ index.js
@@ -5,22 +5,61 @@
 
 const url = `https://opensky-network.org/api`;
 
+// take data from form
+document.getElementById('country-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const country = document.getElementById('country').value.trim().toLowerCase();
+
+    searchByCountry(country);
+});
+
+// receive all flights info
 async function coverCountry() {
     let response = await fetch(`https://opensky-network.org/api/states/all?lamin=45.8389&lomin=5.9962&lamax=47.8229&lomax=10.5226`);
     let data = await response.json();
 
+    // console.log(data);
     return data;
 }
 
+// get valuable informations about flights
 async function displayData() {
     let data = await coverCountry();
+    let timestamp = await data.time;
+    let date = new Date(timestamp * 1000);
+
     let filteredFlights = data.states.map(flight => {
         return {
             callsign: flight[1],
-            velocity: flight[9]
+            originCountry: flight[2],
+            longitude: flight[5],
+            latitude: flight[6],
+            baroAltitude: flight[7],
+            onGround: flight[8],
+            velocity: flight[9],
+            trueTrack: flight[10],
+            timePosition: flight[3],
+            lastContact: flight[4]
         };
     });
-    console.log(filteredFlights);
+    // console.log(date);
+    // console.log(filteredFlights);
+
+    return {filteredFlights, date};
 }
 
-displayData();
+// show flights by Country
+async function searchByCountry(country) {
+    let date = (await displayData()).date;
+    let data = (await displayData()).filteredFlights;
+    console.log(country);
+    data.map(flight => {
+        if (flight["originCountry"].trim().toLowerCase() === country) {
+            console.log(flight);
+        }
+    })
+}
+
+// Refactor code to get it clean
+// Display data in a pleasant way (flight-info + mark on a map)
+// Handle styling
