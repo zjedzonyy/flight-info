a7c7f440b9a70d33dd3bd23e9bdcbfc6f7e44239
zjedzonyy <artompor@gmail.com>
2024-12-09 09:56:06 +0100

    Create map and add function to display drones coordinates.

--- index.js
+++ index.js
@@ -5,17 +5,21 @@
 
 const url = `https://opensky-network.org/api`;
 
-// take data from form
-document.getElementById('country-form').addEventListener('submit', (event) => {
-    event.preventDefault();
-    const country = document.getElementById('country').value.trim().toLowerCase();
+// take data from form and perform API query
+function getFlightInfo() {
+    document.getElementById('country-form').addEventListener('submit', (event) => {
+        event.preventDefault();
+        const country = document.getElementById('country').value.trim().toLowerCase();
+    
+        searchByCountry(country);
+    });
+}
 
-    searchByCountry(country);
-});
+
 
 // receive all flights info
 async function coverCountry() {
-    let response = await fetch(`https://opensky-network.org/api/states/all?lamin=45.8389&lomin=5.9962&lamax=47.8229&lomax=10.5226`);
+    let response = await fetch(`https://opensky-network.org/api/states/all?lamin=35.8389&lomin=2.9962&lamax=67.8229&lomax=25.5226`);
     let data = await response.json();
 
     // console.log(data);
@@ -32,7 +36,7 @@ async function displayData() {
         return {
             callsign: flight[1],
             originCountry: flight[2],
-            longitude: flight[5],
+            longtitude: flight[5],
             latitude: flight[6],
             baroAltitude: flight[7],
             onGround: flight[8],
@@ -43,23 +47,63 @@ async function displayData() {
         };
     });
     // console.log(date);
-    // console.log(filteredFlights);
+    console.log(filteredFlights);
 
     return {filteredFlights, date};
 }
-
+displayData()
 // show flights by Country
 async function searchByCountry(country) {
     let date = (await displayData()).date;
     let data = (await displayData()).filteredFlights;
+    let coordinates = [];
+    
     console.log(country);
     data.map(flight => {
         if (flight["originCountry"].trim().toLowerCase() === country) {
-            console.log(flight);
+            // console.log(flight);
+            coordinates.push([flight["latitude"], flight["longtitude"]])
+            // console.log(coordinates);
         }
     })
+
+    createMap(51, 10.45, coordinates);
 }
 
+
+
+
+// create map
+const lat = 51.165691;
+const long = 10.451526;
+let coordinates = [[50, 10], [52, 11]];
+
+function createMap(lat, long, coordinates) {
+    let map = L.map('map').setView([lat, long], 5);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+    coordinates.forEach(coordinate => {
+        console.log(coordinates)
+        L.marker(coordinate).addTo(map)
+        .bindPopup(`Marker na współrzędnych ${coordinate}`)
+        .openPopup();
+    });
+
+
+}
+
+// createMap(lat, long, coordinates);
+
+
+
+
+getFlightInfo();
+
 // Refactor code to get it clean
+    // searchByCountry() - handle double await together ()
+    // Change: originCountry !== lat, long. 
 // Display data in a pleasant way (flight-info + mark on a map)
 // Handle styling
+
+
